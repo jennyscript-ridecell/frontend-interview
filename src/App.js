6a36ad9573de1e2f4997a72764d85880bb0558cc
Jenny Steele <jenny@ridecell.com>
2017-04-06 10:33:11 -0700

    Connect to satori channel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
+import RTM from 'satori-sdk-js';
+
+const endpoint = "wss://open-data.api.satori.com";
+const appKey = "8a0Dc4D096eC9560FF3DDb0A46849ECb";
+const channel = "transportation";
 
 class App extends Component {
+  componentWillMount() {
+    const rtm = new RTM(endpoint, appKey);
+    rtm.on("enter-connected", function() {
+      console.log("Connected to RTM!");
+    });
+
+    const subscription = rtm.subscribe(channel, RTM.SubscriptionMode.SIMPLE, {
+      filter: "select * from transportation where header.`user-data`='trimet'"
+    });
+    subscription.on('rtm/subscription/data', function (pdu) {
+      pdu.body.messages.forEach(function (msg) {
+        console.log(msg);
+      });
+    });
+
+    rtm.start();
+  }
   render() {
     return (
       <div className="App">
